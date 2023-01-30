452a99c6a201dcd4793cbb77b039b5619457ab86
thfastang401 <thfastang401@naver.com>
2023-01-30 17:56:52 +0900

    230130 한솔

---
+++ src/code_2023/Test.java
@@ -0,0 +1,20 @@
+package test;
+
+//문제 설명
+//정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+//입출력 예 #1
+//
+//num1이 3, num2가 2이므로 3을 2로 나눈 나머지 1을 return 합니다.
+public class Test {
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 2;
+		int answer = -1;
+        answer = num1%num2;
+        System.out.println(answer);;
+
+	}
+
+}
+  
