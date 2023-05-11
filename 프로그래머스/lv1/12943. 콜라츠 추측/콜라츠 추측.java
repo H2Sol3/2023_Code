7ed7285df6ad4e4a9412a8e06f81e66cf5252e87
thfastang401 <46189935+thfastang401@users.noreply.github.com>
2023-05-11 23:02:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,26 +1,24 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
-        int count=1;
-        long num2 = num;
-        
-        
-        for(int i =0;i<500;i++){
-            if(num2==1){
-                 break;
-            }else if(num2 % 2==0){
-                    num2/=2;
-                    answer = count++;
+        long num2=(long)num;
+        for(int i=1;i<=500;i++){
+            if(num2!=1){
+                if(num2%2==0){
+                    num2=num2/2;
                 }else{
                     num2=num2*3+1;
-                    answer = count++;
                 }
+            }else if(num2 == 1){
+                answer = i-1;
+                break;
+            }
             
+            if(i == 500){
+                answer = -1;
+                break;
+            }
         }
-        if(count>=500){
-            answer=-1;
-        }
-        
         
         return answer;
     }
