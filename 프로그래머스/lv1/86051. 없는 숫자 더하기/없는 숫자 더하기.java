a088c49c6e3d69dd0739453491084e02602ba19f
thfastang401 <46189935+thfastang401@users.noreply.github.com>
2023-05-14 00:39:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,17 +1,14 @@
-import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -1;
-        int sum=0;
-        Arrays.sort(numbers); //정렬
-        
-        for(int i=0;i<numbers.length;i++){ // 길이만큼 반복
-           sum+=numbers[i]; //변수에 배열 모든 값 더해주기
+        int answer = 0;
+        int sum =0;
+        for(int i=0;i<numbers.length;i++){
+            answer += numbers[i];
         }
-        
-        answer=45-sum; // 0~9까지의 합은 45. 45에서 sum을 빼주면 없는 숫자의 합이 나온다.
-        
-        
+        for(int i=0;i<=9;i++){
+            sum += i;
+        }
+        answer = sum - answer;
         return answer;
     }
 }
