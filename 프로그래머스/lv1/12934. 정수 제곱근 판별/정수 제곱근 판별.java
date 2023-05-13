b56a8b68a38811b76d4e5e90b91d102baa3f9d5a
thfastang401 <46189935+thfastang401@users.noreply.github.com>
2023-05-13 23:29:27 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,13 +2,13 @@ class Solution {
     public long solution(long n) {
         long answer = 0;
         
-        for(long i=1;i<=n;i++){ //제곱근을 찾을때까지 돌리기
-            if(i*i == n){// 제곱한 숫자와 n이 동일하면
-                answer = (i+1)*(i+1); //제곱 해주기
-                return answer;
+            for(long i = 1; i<=n;i++){
+                if(i*i == n){
+                    answer = (i+1)*(i+1);
+                    return answer;
+                }
             }
-        }
-        
-        return -1;
+        answer = -1;
+        return answer;
     }
 }
