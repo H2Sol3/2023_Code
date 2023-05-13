6bb230e47a4addecb1535bc7fe13fa9c56fee34d
thfastang401 <46189935+thfastang401@users.noreply.github.com>
2023-05-13 23:07:00 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,16 +3,12 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        String s= String.valueOf(n);
+        String s = String.valueOf(n); //문자열로 변환해주기
+        
         for(int i=0;i<s.length();i++){
-   //         System.out.println();
-            answer+=(s.charAt(i)-48);
+            answer += s.charAt(i)-48;//문자열이니까 -48해주기
         }
         
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println(answer);
-
         return answer;
     }
 }
