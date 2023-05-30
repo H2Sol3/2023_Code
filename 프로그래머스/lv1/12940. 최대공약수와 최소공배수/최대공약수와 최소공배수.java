bc7f4a8bd6b66cf183988579df00e6094333bd70
thfastang401 <46189935+thfastang401@users.noreply.github.com>
2023-05-30 22:28:27 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer=new int[2];
+        int mul = n * m;
+        //최대공약수
+        while(n != 0){
+            int tmp1 = m % n; //큰 수를 작은 수로 나누기
+            m = n;
+            n = tmp1;
+        }
+        answer[0] = m;
+        
+        //최소공배수
+        while(m != 0){
+            int tmp2 = n % m;
+            n = m;
+            m = tmp2;
+        }
+        answer[1] = mul / n;
+        return answer;
+    }
+}
