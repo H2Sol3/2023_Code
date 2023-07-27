912ac1eb10e6584e6bb6d2614c8eb0d35f57d372
홍한솔 <46189935+thfastang401@users.noreply.github.com>
2023-07-27 21:24:39 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,14 +1,16 @@
 class Solution {
     public int solution(int[] common) {
-      int answer = 0;
-        //예시처럼 배열 크기가 2는 되야 함
-        if((common[1] - common[0]) == (common[2]-common[1])){
-            //1. 등차수열인지 확인
-            answer = common[common.length-1] + (common[1] - common[0]);
+        int answer = 0;
+        
+        if(common[1]-common[0] == common[2]-common[1]){
+            int num1 = common[1]-common[0];
+            answer = common[common.length-1]+num1;
         }else{
-            //등차수열 아니면 등비수열이니까 그냥 등비수열로 진행
-            answer = common[common.length-1] * (common[1]/common[0]);
-        }        
+            int num2 = common[1]/common[0];
+            answer = common[common.length-1]*num2;
+        }
+            
+        
         return answer;
     }
 }
