b968029db14cdb1f8a8790e19f8e1e394d12024a
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-08-15 21:49:08 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.32 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -2,30 +2,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] sides) {
         int answer = 0;
-   //     int max=Math.max(Math.max(sides[0],sides[1]),sides[2]);   
-   //     for(int i=0;i<sides.length;i++){
-   //         if(sides[i]!=max){
-   //             answer+=sides[i];
-   //         }
-   //     }
-        
-   //     if(answer>max){
-   //         return 1;
-   //     }else{
-   //         return 2;
-   //     }
-        
-        
         Arrays.sort(sides);
-        
-        if( sides[2] < (sides[0] + sides[1]) ) {
+        if(sides[0]+sides[1] > sides[2]){
             answer = 1;
-        } else {
-            answer = 2;
-        }
-        
-        
+        }else answer = 2;
         return answer;
-        
     }
 }
