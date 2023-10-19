1bc02ba4b7a0d9811a6e0864ee9ca5c24f629f4c
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-10-20 01:05:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,16 +4,15 @@ public class Main{
         Scanner scan = new Scanner(System.in);
         int gram = scan.nextInt();
         int cnt = 0;
+        
         while(true){
-            //5로 나눠질 경우
             if(gram % 5 == 0){
                 cnt += gram / 5;
                 break;
             }
-            //아닐 때는
             gram -= 3;
             cnt++;
-
+            
             if(gram < 0){
                 cnt = -1;
                 break;
@@ -21,5 +20,4 @@ public class Main{
         }
         System.out.println(cnt);
     }
-
 }
