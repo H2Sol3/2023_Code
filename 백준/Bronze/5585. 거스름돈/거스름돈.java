b1b219228d0e73c144783875cfd4ed2b55704b0d
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-10-23 00:12:22 +0900

    [Bronze II] Title: 거스름돈, Time: 204 ms, Memory: 17528 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -3,38 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        int n = 1000-scan.nextInt();
-        int cnt = 0;
         
-        while (n > 0) {
-            if (n >= 500) {
-                cnt += n / 500;
-                n = n % 500;
-            }
-            if (n >= 100) {
-                cnt += n / 100;
-                n = n % 100;
-            }
-            if (n >= 50) {
-                cnt += n / 50;
-                n = n % 50;
-            }
-            if (n >= 10) {
-                cnt += n / 10;
-                n = n % 10;
-            }
-            if (n >= 5) {
-                cnt += n / 5;
-                n = n % 5;
-            }
-            if (n >= 1) {
-                cnt += n;
-                n = n % 1;
-            }
-        }
-        System.out.print(cnt);
+        int n = 1000 - scan.nextInt();
+        int cnt = 0;
+        int[] coinValues = {500, 100, 50, 10, 5, 1};
 
+        for(int i=0;i<coinValues.length;i++){
+            cnt += n / coinValues[i];
+            n = n % coinValues[i];
+        }
+        
+        System.out.print(cnt);
     }
 
 }
