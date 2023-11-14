484313c8d34251c4d9060d0600b420c02ea85020
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-11-14 19:28:48 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2];
+        
+        for(int i=0;i<keyinput.length;i++){
+            switch(keyinput[i]){
+                case "up": 
+                    if(answer[1] == board[1]/2){
+                        break;
+                    }else{
+                        answer[1] += 1;
+                        break;
+                    }
+                case "down":  
+                    if(answer[1] == -board[1]/2){
+                        break;
+                    }else{
+                        answer[1] -= 1;
+                        break;
+                    }
+                case "left": 
+                    if(answer[0] == -board[0]/2){
+                        break;
+                    }else{
+                        answer[0] -= 1;
+                        break;
+                    }
+                case "right": 
+                    if(answer[0] == board[0]/2){
+                        break;
+                    }else{
+                        answer[0] += 1;
+                        break;
+                    }
+            }
+        }
+        
+        return answer;
+    }
+}
