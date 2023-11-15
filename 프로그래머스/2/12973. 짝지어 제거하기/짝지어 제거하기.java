171bb04d8379665cc2264f0185fcaae737d1f3b0
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-11-15 23:03:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 47.22 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String[] args){
+        solution("baabaa");
+        solution("cdcd");
+    }
+    public static int solution(String s)
+    {
+        int answer = 0;
+        Stack<Character> stack = new Stack();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(!stack.empty() && stack.peek() == ch){
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+        }
+               
+        if(stack.empty()){
+            answer = 1;
+        }
+        return answer;
+    }
+}
