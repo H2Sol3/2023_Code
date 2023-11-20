f7a792df0e51734019548450483891ab1dae3944
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-11-20 23:32:07 +0900

    [Bronze III] Title: 약수 구하기, Time: 228 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+
+        if (list.size() < k) {
+            System.out.println(0);
+        } else {
+            System.out.println(list.get(k - 1));
+        }
+    }
+}
