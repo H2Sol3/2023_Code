c559bf5d1a7af1aa0ca0102c99a9408ba3639cbd
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-12-11 00:27:45 +0900

    [level 2] Title: 올바른 괄호, Time: 18.66 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        if(s.charAt(0) == ')'){
+            answer = false;
+        }else{
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i) == '('){
+                    stack.push('(');
+                }else{
+                    if(stack.size() == 0){
+                        answer = false;
+                        break;
+                    }
+                    
+                    stack.pop();
+                }
+            }
+        }
+        
+        
+        if(stack.size() != 0){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
