e119c5bb694654bc3832311a6f73968f48d67715
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-12-12 18:29:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 11.14 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        char su = '수';
+        char bak = '박';
+        
+        for(int i=1;i<=n;i++){
+            if(i%2 != 0){
+                answer+=su;
+            }else{
+                answer+=bak;
+            }
+        }
+        return answer;
+    }
+}
