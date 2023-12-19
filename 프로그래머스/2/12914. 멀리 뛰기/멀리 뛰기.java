d0be3d4ed715afde7cbd3c9b6d1098a8c36165ca
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-12-19 23:29:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,20 +1,16 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        int dp[] = new int[2001];
+        int[] dp = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
         
-        dp[0] = 1;
-        dp[1] = 2;
-        
-        if(n>=3){
-            for(int i=2;i<2001;i++){
-                dp[i] = (dp[i-1]+dp[i-2]) % 1234567;
-            }
+        for(int i=4;i<dp.length;i++){
+            dp[i] = (dp[i-1]+dp[i-2])%1234567;
         }
         
-        answer= dp[n-1];
-        
-        
+        answer = dp[n];
         return answer;
     }
 }
