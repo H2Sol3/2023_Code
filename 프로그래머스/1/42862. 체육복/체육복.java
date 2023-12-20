ef066b88e6f7c3e0951714289e2460e5bcb0f355
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2023-12-20 19:45:56 +0900

    [level 1] Title: 체육복, Time: 0.39 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,42 +1,41 @@
 import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int cnt = 0;
-        
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
+        //lost = reserve
         for(int i=0;i<lost.length;i++){
             for(int j=0;j<reserve.length;j++){
                 if(lost[i] == reserve[j]){
-                    cnt++;
-                    lost[i] = -3;
-                    reserve[j] = -1;
+                    lost[i] = -1;
+                    reserve[j] = -3;
+                    answer++;
                     break;
                 }
             }
         }
         
+        //다른 학생들
         for(int i=0;i<lost.length;i++){
-            for(int j=0;j<reserve.length;j++){
-                
-                int lostFrontNum = lost[i]-1;
-                int lostBackNum = lost[i]+1;
-                
-                if(reserve[j] == -1){
-                    continue;
-                }else if(lostFrontNum == reserve[j] || lostBackNum == reserve[j]){
-                    cnt++;
-                    reserve[j] = -1;
-                    break;
+                for(int j=0;j<reserve.length;j++){
+                    if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j]){
+                        lost[i] = -1;
+                        reserve[j] = -3;
+                        answer++;
+                    }
                 }
-                
-            }
         }
+        //잃어버리지 않은 학생수 + answer
+        // 4 = n - lost + answer
+        //5 [4,5] [3,4] cnt 1  5 - lost + answer
+        //System.out.println(n);
+       // System.out.println(lost.length);
+        //System.out.println(answer);
         
-        answer = n-lost.length+cnt;
+        answer = n - lost.length + answer;
+        
         
         return answer;
     }
