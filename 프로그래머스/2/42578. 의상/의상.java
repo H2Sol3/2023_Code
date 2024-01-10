f69137a9eb3ecf39f4c63218f0aada2055b847d3
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-01-10 22:23:23 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,16 +4,16 @@ class Solution {
         int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
         
-        //옷 넣기
+        //옷 넣기, 옷 있으면 1
         for(int i=0;i<clothes.length;i++){
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0) + 1);
         }
         
-        //조합
-        for(String key : map.keySet()){
-            answer *= map.get(key)+1;
+        //옷 조합하기
+        for(String s : map.keySet()){
+            answer = answer * (map.get(s)+1);
         }
-        answer = answer-1;
+        answer = answer - 1;
         return answer;
     }
 }
