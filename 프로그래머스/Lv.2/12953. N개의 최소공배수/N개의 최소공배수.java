09772004989b51dcc79a9a761cdf98a9a042aa4f
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-01-14 23:24:00 +0900

    [level Lv.2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            int a = answer;
+            int b = arr[i];
+
+            while (b != 0) {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+
+            answer = (answer * arr[i]) / a;
+        }
+        
+        
+        return answer;
+    }
+}
