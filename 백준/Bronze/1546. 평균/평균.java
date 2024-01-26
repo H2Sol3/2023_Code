bfbb4e1eb5edeb0aa208ddf41674e98ebc87e98c
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-01-27 00:44:37 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,21 +1,28 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        double avg = 0.0;
         int max = 0;
-        double avg = 0;
-
-        for(int i=0;i<arr.length;i++){
-            arr[i] = scan.nextInt();
-            avg+=arr[i];
-            max = Math.max(arr[i], max);
+        double sum = 0.0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<n;i++){
+            //list.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
         }
-
-        avg = avg/arr.length;
-        double newAvg = (double)avg/max*100;
-
-        System.out.println(newAvg);
-    }
+        
+        for(int i=0;i<n;i++){
+            //sum += list.get(i)/list.get(0)*100;
+            sum += (double)arr[i]/max*100;
+        }
+        avg = sum/n;
+        System.out.println(avg);
+    }   
 }
