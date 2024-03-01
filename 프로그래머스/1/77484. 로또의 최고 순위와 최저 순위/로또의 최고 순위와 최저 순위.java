e8a487c6ca7836cfc6d7aacc81c12447ffdc7459
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-03-01 23:53:14 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.38 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
+        int[] lottoNum = {6,6,5,4,3,2,1};
         int cnt = 0;
         int zero = 0;
         //같은 번호 찾기
@@ -15,62 +16,15 @@ class Solution {
             
             for(int j=0;j<6;j++){
                 if(lottos[i] == win_nums[j]){
-                    win_nums[j] = -1;
                     cnt++;
                     break;
                 }
             }
         }
         
-        if(cnt == 6){
-            answer[0] = 1;
-            answer[1] = 1;
-        }else{
-            //모두 일치 => cnt + zero
-            switch(cnt+zero){
-                case 6 :
-                    answer[0] = 1;
-                    break;
-                case 5 :
-                    answer[0] = 2;
-                    break;
-                case 4 :
-                    answer[0] = 3;
-                    break;
-                case 3 :
-                    answer[0] = 4;
-                    break;
-                case 2 :
-                    answer[0] = 5;
-                    break;
-                default :
-                    answer[0] = 6;
-                    break;
-            }
-            
-            //모두 일치x => cnt
-            switch(cnt){
-                case 6 :
-                    answer[1] = 1;
-                    break;
-                case 5 :
-                    answer[1] = 2;
-                    break;
-                case 4 :
-                    answer[1] = 3;
-                    break;
-                case 3 :
-                    answer[1] = 4;
-                    break;
-                case 2 :
-                    answer[1] = 5;
-                    break;
-                default :
-                    answer[1] = 6;
-                    break;
-            }
-            
-        }
+        answer[0] = lottoNum[zero+cnt];
+        answer[1] = lottoNum[cnt];
+        
         
         return answer;
     }
