ed0958d33964d360fe542011631117db03ee8e1c
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-05-04 21:35:14 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++ 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,4 +1,5 @@
-select count(*) FISH_COUNT from fish_info i
+select count(*) FISH_COUNT 
+from fish_info i
 join fish_name_info n
 on i.fish_type = n.fish_type
 where fish_name in('BASS','SNAPPER');
