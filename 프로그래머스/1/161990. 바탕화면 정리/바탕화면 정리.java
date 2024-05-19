6514fc57f3e89ab980e9163db8ac90d73f3f7671
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-05-19 14:51:18 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.47 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = {wallpaper.length,wallpaper[0].length(),0,0};
+        
         for(int c=0;c<wallpaper.length;c++){
             for(int r=0;r<wallpaper[0].length();r++){
                 if(wallpaper[c].substring(r,r+1).equals("#")){
+                    
                     answer[0] = Math.min(answer[0],c);
                     answer[1] = Math.min(answer[1],r);
                     answer[2] = Math.max(answer[2],c);
@@ -11,8 +13,10 @@ class Solution {
                 }
             }
         }
+        
         answer[2] +=1;
         answer[3] +=1;
+        
         return answer;
     }
 }
