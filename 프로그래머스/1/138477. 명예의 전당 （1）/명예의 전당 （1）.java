e3df6f0e59c8c402de79ee2f6745fd9b95587c18
HanSol Hong <46189935+H2Sol3@users.noreply.github.com>
2024-06-02 20:23:13 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.29 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        //리스트 생성
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        //k번째까지 리스트 넣고, 정렬(마지막 값은 answer에 넣어주기)
+        for(int i=0;i<score.length;i++){
+        
+            list.add(score[i]);
+            Collections.sort(list); 
+            
+            //역순으로 정렬해주기
+            Collections.reverse(list);
+            
+            if(i == 0){
+                answer[i]=list.get(i);    
+            }else if(i<k){
+                answer[i]=list.get(i);    
+            }else{
+                answer[i]=list.get(k-1);
+            }
+            
+        }
+        
+        return answer;
+    }
+}
